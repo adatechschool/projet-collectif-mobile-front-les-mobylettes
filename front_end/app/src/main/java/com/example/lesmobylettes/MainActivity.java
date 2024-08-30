a596ae3a8f92b953ad7007a1924479035a0a46e8
Votre Nom <h.hassen.malek@gmail.com>
2024-08-30 10:54:37 +0200

    mise à jour de MAinActivity.java, création des boutons et ajout de la photo la bonne soeurf dans le fond

--- front_end/app/src/main/java/com/example/lesmobylettes/MainActivity.java
+++ front_end/app/src/main/java/com/example/lesmobylettes/MainActivity.java
@@ -1,27 +1,35 @@
 package com.example.lesmobylettes;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.graphics.Typeface;
+import androidx.core.content.res.ResourcesCompat;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+//import androidx.navigation.NavController;
+//import androidx.navigation.Navigation;
+//import androidx.navigation.ui.AppBarConfiguration;
+//import androidx.navigation.ui.NavigationUI;
 
 import com.example.lesmobylettes.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
+//import android.view.Menu;
+//import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
+   // private AppBarConfiguration appBarConfiguration;
+
     private ActivityMainBinding binding;
+
+    private TextView textView;
+
+    private Button btnRegister;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,48 +38,65 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        textView = findViewById(R.id.textView);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.poppins_regular);
+        textView.setTypeface(typeface);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnLogin);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+
+        //setSupportActionBar(binding.toolbar);
+
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+       // appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                // Action pour le bouton Register
+                Toast.makeText(MainActivity.this, "Register button clicked", Toast.LENGTH_SHORT).show();
+                // Ajoutez ici la logique pour l'inscription
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Action pour le bouton Login
+                Toast.makeText(MainActivity.this, "Login button clicked", Toast.LENGTH_SHORT).show();
+                // Ajoutez ici la logique pour la connexion
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+   // @Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+        //// Inflate the menu; this adds items to the action bar if it is present.
+      //  getMenuInflater().inflate(R.menu.menu_main, menu);
+       // return true;
+   // }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+   // @Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+      //  int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+         //   return true;
+        //}
 
-        return super.onOptionsItemSelected(item);
-    }
+      //  return super.onOptionsItemSelected(item);
+    //}
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+    //@Override
+    //public boolean onSupportNavigateUp() {
+      //  NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+       // return NavigationUI.navigateUp(navController, appBarConfiguration)
+         //       || super.onSupportNavigateUp();
+    //}
 }
