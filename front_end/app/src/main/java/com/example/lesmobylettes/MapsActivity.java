ef4e6f3ba61a08b06e9c923f43cd91ea5eef01cc
nora <noraidris621@gmail.com>
2024-09-05 14:52:43 +0200

    adding uses-permission to manifest

--- front_end/app/src/main/java/com/example/lesmobylettes/MapsActivity.java
+++ front_end/app/src/main/java/com/example/lesmobylettes/MapsActivity.java
@@ -3,14 +3,12 @@ package com.example.lesmobylettes;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.fragment.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-/*import com.example.lesmobylettes.R;*/
 import java.util.List;
 
 
